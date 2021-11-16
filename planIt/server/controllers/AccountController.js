df7a55c5740e428797313959590326ffefb208f4
Tristan Johns <tfj@protonmail.com>
2021-11-16 11:28:41 -0700

    account

--- planIt/server/controllers/AccountController.js
+++ planIt/server/controllers/AccountController.js
@@ -1,13 +1,15 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
 import BaseController from '../utils/BaseController'
+import { logger } from '../utils/Logger'
 
 export class AccountController extends BaseController {
   constructor() {
-    super('account')
+    super('api/account')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
+      .put('/:accountId', this.edit)
   }
 
   async getUserAccount(req, res, next) {
@@ -16,6 +18,17 @@ export class AccountController extends BaseController {
       res.send(account)
     } catch (error) {
       next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const account = await accountService.updateAccount(req.body)
+      logger.log(account)
+      return res.send(account)
+    } catch (e) {
+      next(e)
     }
   }
 }
