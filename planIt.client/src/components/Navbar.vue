d747ff204062b6b2109a442c9bf7354407df3df5
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2021-11-18 11:11:43 -0700

    Style

--- planIt.client/src/components/Navbar.vue
+++ planIt.client/src/components/Navbar.vue
@@ -1,17 +1,11 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark lighten-10 px-3">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-light lighten-10 px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <img src="../assets/img/rocket.png" height="65" alt="Logo" />
+      <h1 class="grad px-2 mb-0 mt-2 align-self-center">PlanIt</h1>
     </router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon" />
     </button>
     <!-- <a
@@ -23,74 +17,44 @@
     >
       Link with href
     </a> -->
-    <button
-      class="btn btn-primary"
-      type="button"
-      data-bs-toggle="offcanvas"
-      data-bs-target="#offcanvasExample"
-      aria-controls="offcanvasExample"
-    >
+    <!-- <button class="btn btngrad selectable" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+      aria-controls="offcanvasExample">
       Projects
-    </button>
+    </button> -->
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link
-            :to="{ name: 'About' }"
-            class="btn text-success lighten-30 selectable text-uppercase"
-          >
-            About
-          </router-link>
+
         </li>
       </ul>
       <span class="navbar-text">
-        <button
-          class="
+        <button class="
             btn
             selectable
-            text-success
+            text-dark
             lighten-30
             text-uppercase
             my-2 my-lg-0
-          "
-          @click="login"
-          v-if="!user.isAuthenticated"
-        >
+          " @click="login" v-if="!user.isAuthenticated">
           Login
         </button>
 
         <div class="dropdown my-2 my-lg-0" v-else>
-          <div
-            class="dropdown-toggle selectable"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-            id="authDropdown"
-          >
-            <img
-              :src="user.picture"
-              alt="user photo"
-              height="40"
-              class="rounded"
-            />
-            <span class="mx-3 text-success lighten-30">{{ user.name }}</span>
+          <div class="dropdown-toggle selectable" data-bs-toggle="dropdown" aria-expanded="false" id="authDropdown">
+            <img :src="user.picture" alt="user photo" height="40" class="rounded" />
+            <span class="mx-3 text-dark lighten-30">{{ user.name }}</span>
           </div>
-          <div
-            class="dropdown-menu p-0 list-group w-100"
-            aria-labelledby="authDropdown"
-          >
+          <div class="dropdown-menu p-0 list-group w-100" aria-labelledby="authDropdown">
             <router-link :to="{ name: 'Account' }">
               <div class="list-group-item list-group-item-action hoverable">
                 Manage Account
               </div>
             </router-link>
-            <div
-              class="
+            <div class="
                 list-group-item list-group-item-action
                 hoverable
                 text-danger
-              "
-              @click="logout"
-            >
+              " @click="logout">
               <i class="mdi mdi-logout"></i>
               logout
             </div>
@@ -103,49 +67,77 @@
 </template>
 
 <script>
-import { AuthService } from '../services/AuthService'
-import { AppState } from '../AppState'
-import { computed, onMounted } from 'vue'
-import { logger } from "../utils/Logger"
-import Pop from "../utils/Pop"
-import { projectService } from "../services/ProjectService"
-export default {
-  setup() {
-    return {
-      user: computed(() => AppState.user),
-      async login() {
-        AuthService.loginWithPopup()
-      },
-      async logout() {
-        AuthService.logout({ returnTo: window.location.origin })
+  import { AuthService } from '../services/AuthService'
+  import { AppState } from '../AppState'
+  import { computed, onMounted } from 'vue'
+  import { logger } from "../utils/Logger"
+  import Pop from "../utils/Pop"
+  import { projectService } from "../services/ProjectService"
+  export default {
+    setup() {
+      return {
+        user: computed(() => AppState.user),
+        async login() {
+          AuthService.loginWithPopup()
+        },
+        async logout() {
+          AuthService.logout({ returnTo: window.location.origin })
+        }
       }
     }
   }
-}
 </script>
 
 <style scoped>
-.dropdown-menu {
-  user-select: none;
-  display: block;
-  transform: scale(0);
-  transition: all 0.15s linear;
-}
-.dropdown-menu.show {
-  transform: scale(1);
-}
-.hoverable {
-  cursor: pointer;
-}
-a:hover {
-  text-decoration: none;
-}
-.nav-link {
-  text-transform: uppercase;
-}
-.navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-}
+  .dropdown-menu {
+    user-select: none;
+    display: block;
+    transform: scale(0);
+    transition: all 0.15s linear;
+  }
+
+  .dropdown-menu.show {
+    transform: scale(1);
+  }
+
+  .hoverable {
+    cursor: pointer;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+  .nav-link {
+    text-transform: uppercase;
+  }
+
+  .navbar-nav .router-link-exact-active {
+    border-bottom: 2px solid var(--bs-success);
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  h1 {
+    font-size: 3rem;
+    font-style: italic;
+    background: linear-gradient(180deg, rgba(8, 37, 243, 1) 4%, rgba(243, 140, 12, 1) 98%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    -webkit-text-fill-color: transparent;
+    font-family: 'Bebas Neue', cursive;
+  }
+
+  .btngrad {
+    font-style: italic;
+    background: linear-gradient(180deg, rgba(8, 37, 243, 1) 4%, rgba(243, 140, 12, 1) 98%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    -webkit-text-fill-color: transparent;
+    font-family: 'Bebas Neue', cursive;
+  }
 </style>
--- planIt.client/src/pages/ProjectPage.vue
+++ planIt.client/src/pages/ProjectPage.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="grad"></div>
+  <button class="btn btngrad2 text-light selectable mt-3" type="button" data-bs-toggle="offcanvas"
+    data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+    Projects
+  </button>
   <div class="component p-3" v-if="active">
     <h1>{{ active.name }}</h1>
     <p>
@@ -7,57 +12,76 @@
     <small>Creator: {{ active.creator.name }}</small>
   </div>
   <button class="mdi mdi-delete" @click="remove(active.id)"></button>
-  <button
-    class="btn btn-rounded btn-dark mdi mdi-plus"
-    data-bs-toggle="modal"
-    data-bs-target="#createSprint"
-  ></button>
+  <button class="btn btn-rounded ms-3 btnGrad mdi mdi-plus" data-bs-toggle="modal"
+    data-bs-target="#createSprint"></button>
   <Sprint />
   <CreateSprint />
 </template>
 
 
 <script>
-import { computed } from "@vue/reactivity"
-import { AppState } from "../AppState"
-import { projectService } from "../services/ProjectService"
-import Pop from "../utils/Pop"
-import { logger } from "../utils/Logger"
-import { router } from "../router"
-import { onMounted } from "@vue/runtime-core"
-import { useRoute } from "vue-router"
-export default {
-  setup() {
-    const route = useRoute()
-    onMounted(async () => {
-      try {
-        await projectService.getAll('api/projects')
-      } catch (error) {
-        logger.error(error)
-      }
-    })
-    return {
-      active: computed(() => AppState.projects.find(p => p.id == route.params.projectId)),
-      sprints: computed(() => AppState.sprints),
-
-      async remove(id) {
+  import { computed } from "@vue/reactivity"
+  import { AppState } from "../AppState"
+  import { projectService } from "../services/ProjectService"
+  import Pop from "../utils/Pop"
+  import { logger } from "../utils/Logger"
+  import { router } from "../router"
+  import { onMounted } from "@vue/runtime-core"
+  import { useRoute } from "vue-router"
+  export default {
+    setup() {
+      const route = useRoute()
+      onMounted(async () => {
         try {
-          logger.log('removed')
-          await projectService.remove(id);
-          router.push({
-            name: "Home",
-          })
+          await projectService.getAll('api/projects')
         } catch (error) {
-          Pop.toast(error.message, "error");
+          logger.error(error)
+        }
+      })
+      return {
+        active: computed(() => AppState.projects.find(p => p.id == route.params.projectId)),
+        sprints: computed(() => AppState.sprints),
+
+        async remove(id) {
+          try {
+            logger.log('removed')
+            await projectService.remove(id);
+            router.push({
+              name: "Home",
+            })
+          } catch (error) {
+            Pop.toast(error.message, "error");
+          }
         }
       }
     }
+
+
   }
-
-
-}
 </script>
 
 
 <style lang="scss" scoped>
+  .btnGrad {
+    background: rgb(145, 8, 243);
+    background: linear-gradient(90deg, rgba(145, 8, 243, 1) 4%, rgba(243, 140, 12, 1) 98%);
+  }
+
+  .grad {
+    width: 100%;
+    height: 4px;
+    background: rgb(145, 8, 243);
+    background: linear-gradient(90deg, rgba(145, 8, 243, 1) 4%, rgba(243, 140, 12, 1) 98%);
+  }
+
+  .btngrad2 {
+    font-style: italic;
+    background: linear-gradient(180deg, rgba(8, 37, 243, 1) 4%, rgba(243, 140, 12, 1) 98%);
+    /* background-clip: text;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    -webkit-text-fill-color: transparent; */
+    font-family: 'Bebas Neue', cursive;
+  }
 </style>
