79088d80ca79555e5212a5a00dae8425ab0c6a04
Tristan Johns <tfj@protonmail.com>
2021-11-19 11:12:06 -0700

    linter

--- planIt.client/src/pages/ProjectPage.vue
+++ planIt.client/src/pages/ProjectPage.vue
@@ -3,9 +3,15 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-1 p-0 m-0">
-        <div class="btngrad2 bg-cDark text-center text-warning selectable mt-3" type="button" data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" aria-label="Open Projects Canvas"
-          title="Open Projects Canvas">
+        <div
+          class="btngrad2 bg-cDark text-center text-warning selectable mt-3"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasExample"
+          aria-controls="offcanvasExample"
+          aria-label="Open Projects Canvas"
+          title="Open Projects Canvas"
+        >
           P
         </div>
       </div>
@@ -19,10 +25,13 @@
             <small>Created By: {{ active.creator.name }}</small>
           </div>
           <div class="col-md-3">
-            <button class="mdi mdi-delete-forever f-24 btn text-danger" @click="remove(active.id)"
-              aria-label="Remove Project" title="Remove project"></button>
+            <button
+              class="mdi mdi-delete-forever f-24 btn text-danger"
+              @click="remove(active.id)"
+              aria-label="Remove Project"
+              title="Remove project"
+            ></button>
           </div>
-
         </div>
         <div class="row"></div>
       </div>
@@ -32,13 +41,18 @@
           <div class="col-md-8 ms-3">
             <h4>Sprints</h4>
             <h6>
-              Group your tasks into sprints for over-arching collections for better
-              organization
+              Group your tasks into sprints for over-arching collections for
+              better organization
             </h6>
           </div>
           <div class="col-md-3 pt-3 text-end">
-            <button class="btn btn-rounded btn-dark mdi mdi-plus ms-3" data-bs-toggle="modal"
-              data-bs-target="#createSprint" aria-label="Create Sprint" title="Create new Sprint">
+            <button
+              class="btn btn-rounded btn-dark mdi mdi-plus ms-3"
+              data-bs-toggle="modal"
+              data-bs-target="#createSprint"
+              aria-label="Create Sprint"
+              title="Create new Sprint"
+            >
               Create Sprint
             </button>
           </div>
@@ -61,80 +75,84 @@
 
 <script>
 
-  import { computed } from "@vue/reactivity"
-  import { AppState } from "../AppState"
-  import { projectService } from "../services/ProjectService"
-  import Pop from "../utils/Pop"
-  import { logger } from "../utils/Logger"
-  import { router } from "../router"
-  import { onMounted } from "@vue/runtime-core"
-  import { useRoute } from "vue-router"
-  import { sprintService } from "../services/SprintService"
-  export default {
-    setup() {
-      const route = useRoute()
-      onMounted(async () => {
+import { computed } from "@vue/reactivity"
+import { AppState } from "../AppState"
+import { projectService } from "../services/ProjectService"
+import Pop from "../utils/Pop"
+import { logger } from "../utils/Logger"
+import { router } from "../router"
+import { onMounted } from "@vue/runtime-core"
+import { useRoute } from "vue-router"
+import { sprintService } from "../services/SprintService"
+export default {
+  setup() {
+    const route = useRoute()
+    onMounted(async () => {
 
+      try {
+        await projectService.getAll('api/projects')
+        await sprintService.getAll('api/projects/' + route.params.projectId + '/sprints')
+      } catch (error) {
+        logger.error(error)
+      }
+    })
+    return {
+      active: computed(() => AppState.projects.find(p => p.id == route.params.projectId)),
+      sprints: computed(() => AppState.sprints),
+
+      async remove(id) {
         try {
-          await projectService.getAll('api/projects')
-          await sprintService.getAll('api/projects/' + route.params.projectId + '/sprints')
+          logger.log('removed')
+          await projectService.remove(id);
+          router.push({
+            name: "Home",
+          })
         } catch (error) {
-          logger.error(error)
-        }
-      })
-      return {
-        active: computed(() => AppState.projects.find(p => p.id == route.params.projectId)),
-        sprints: computed(() => AppState.sprints),
-
-        async remove(id) {
-          try {
-            logger.log('removed')
-            await projectService.remove(id);
-            router.push({
-              name: "Home",
-            })
-          } catch (error) {
-            Pop.toast(error.message, "error");
-          }
+          Pop.toast(error.message, "error");
         }
       }
     }
-
-
   }
+
+
+}
 </script>
 
 
 <style lang="scss" scoped>
-  .btnGrad {
-    background: rgb(145, 8, 243);
-    background: linear-gradient(90deg,
-        rgba(145, 8, 243, 1) 4%,
-        rgba(243, 140, 12, 1) 98%);
-  }
+.btnGrad {
+  background: rgb(145, 8, 243);
+  background: linear-gradient(
+    90deg,
+    rgba(145, 8, 243, 1) 4%,
+    rgba(243, 140, 12, 1) 98%
+  );
+}
 
-  .grad {
-    width: 100%;
-    height: 4px;
-    background: rgb(145, 8, 243);
-    background: linear-gradient(90deg,
-        rgba(145, 8, 243, 1) 4%,
-        rgba(243, 140, 12, 1) 98%);
-  }
+.grad {
+  width: 100%;
+  height: 4px;
+  background: rgb(145, 8, 243);
+  background: linear-gradient(
+    90deg,
+    rgba(145, 8, 243, 1) 4%,
+    rgba(243, 140, 12, 1) 98%
+  );
+}
 
-  .btngrad2 {
-    font-style: italic;
-    /* background: linear-gradient(180deg,
+.btngrad2 {
+  font-style: italic;
+  /* background: linear-gradient(180deg,
         rgba(8, 37, 243, 1) 4%,
         rgba(243, 140, 12, 1) 98%); */
-    /* background-clip: text;
+  /* background-clip: text;
     -webkit-background-clip: text;
     -moz-background-clip: text;
     -moz-text-fill-color: transparent;
     -webkit-text-fill-color: transparent; */
-    font-family: "Bebas Neue", cursive;
-    max-width: 65%;
-    height: 60px;
-    font-size: 3em;
-  }
+  font-family: "Bebas Neue", cursive;
+  max-width: 65%;
+  height: 60px;
+  font-size: 3em;
+}
 </style>
