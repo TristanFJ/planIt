d1f1f5a242d7c81bb392675df9288e1109552325
Ryan Fitzgerald <ryfitz@ymail.com>
2021-11-18 21:25:54 -0700

    sprints fixed

--- planIt.client/src/components/Sprint.vue
+++ planIt.client/src/components/Sprint.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="sprint p-3">
     <div class="mt-5">
-
-      <div class="container card m-2 w-50">
+      <div class="container card m-2">
         <div class="row d-flex justify-content-between">
           <div class="col-md-3 m-0 p-0">
-            <i @click="remove()" class="
-              selectable
-              bg-danger
-              mdi mdi-close
-              rounded-circle
-              align-self-center
-
-            "></i>
+            <i
+              @click="remove()"
+              class="
+                selectable
+                bg-danger
+                mdi mdi-close
+                rounded-circle
+                align-self-center
+              "
+            ></i>
           </div>
           <div class="col-md-6 p-3">
-            <h4>{{ sprint.name }} </h4>
+            <h4>{{ sprint.name }}</h4>
           </div>
           <div class="col-md-3 p-3 text-end d-flex justify-content-end">
-            <i class="mdi mdi-weight mx-2 ">
-            </i>
+            <i class="mdi mdi-weight mx-2"> </i>
             <p>{{ weight }}</p>
           </div>
         </div>
@@ -30,17 +30,38 @@
         <form @submit.prevent="createTask()">
           <div class="row">
             <div class="col-8">
-              <input v-model="editable.name" type="text" class="form-control" placeholder="Task Name"
-                aria-label="Example text with button addon" aria-describedby="button-addon1" required />
+              <input
+                v-model="editable.name"
+                type="text"
+                class="form-control"
+                placeholder="Task Name"
+                aria-label="Example text with button addon"
+                aria-describedby="button-addon1"
+                required
+              />
             </div>
             <div class="col-4">
-              <input v-model="editable.weight" type="number" min="1" max="5" pattern=".{1, 5}" class="form-control"
-                placeholder="Weight" aria-label="Example text with button addon" aria-describedby="button-addon1"
-                required />
+              <input
+                v-model="editable.weight"
+                type="number"
+                min="1"
+                max="5"
+                pattern=".{1, 5}"
+                class="form-control"
+                placeholder="Weight"
+                aria-label="Example text with button addon"
+                aria-describedby="button-addon1"
+                required
+              />
             </div>
           </div>
-          <button type="submit" @click.prevent="createTask()" class="btn btn-primary" data-bs-target="#createSprint"
-            data-bs-dismiss="modal">
+          <button
+            type="submit"
+            @click.prevent="createTask()"
+            class="btn btn-primary"
+            data-bs-target="#createSprint"
+            data-bs-dismiss="modal"
+          >
             Create
           </button>
         </form>
@@ -52,86 +73,86 @@
 
 <script>
 
-  import { computed, onMounted, reactive, ref, watchEffect } from "@vue/runtime-core"
-  import { AppState } from "../AppState"
-  import { logger } from "../utils/Logger"
-  import { sprintService } from "../services/SprintService"
+import { computed, onMounted, reactive, ref, watchEffect } from "@vue/runtime-core"
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { sprintService } from "../services/SprintService"
 
 
-  import Pop from "../utils/Pop"
-  import { useRoute } from "vue-router"
-  import { taskService } from "../services/TaskService"
-  export default {
-    props: { sprint: { type: Object, required: true } },
-    setup(props) {
+import Pop from "../utils/Pop"
+import { useRoute } from "vue-router"
+import { taskService } from "../services/TaskService"
+export default {
+  props: { sprint: { type: Object, required: true } },
+  setup(props) {
 
-      const route = useRoute()
-      let editable = ref({
-      });
-
-
-      // REVIEW watcheffect???
-      onMounted(async () => {
-        try {
-          await taskService.getAll('api/projects/' + route.params.projectId + '/tasks')
-          // AppState.tasks.forEach(t => {
-          //   if (t.sprintId == props.sprint.id) {
-          //     weight.value += t.weight
-          //   }
-          // })
-        } catch (error) {
-          logger.error(error);
-          Pop.toast(error.message, 'error')
-        }
-      })
-      return {
-        editable,
-        active: computed(() => AppState.activeProject),
-        tasks: computed(() => AppState.tasks.filter(t => t.sprintId == props.sprint.id)),
-        weight: computed(() => {
-          let weight = 0
-          AppState.tasks.forEach(t => {
-            if (t.sprintId == props.sprint.id) {
-              weight += t.weight
-            }
-          })
-          return weight
-        }),
+    const route = useRoute()
+    let editable = ref({
+    });
 
 
-        async remove() {
-          try {
-            const sprintId = props.sprint.id
-            await sprintService.remove(sprintId, route.params.projectId)
-          } catch (error) {
-            logger.error(error)
-          }
-
-        },
-
-        async createTask() {
-          try {
-            editable.value.sprintId = props.sprint.id
-            await taskService.createTask(route.params.projectId, editable.value)
-            // logger.log('createTask', route.params.projectId, editable.value)
-            Pop.toast('created', 'success')
-            editable.value = {}
-          } catch (error) {
-            logger.error(error)
-          }
-        },
-
-
-
+    // REVIEW watcheffect???
+    onMounted(async () => {
+      try {
+        await taskService.getAll('api/projects/' + route.params.projectId + '/tasks')
+        // AppState.tasks.forEach(t => {
+        //   if (t.sprintId == props.sprint.id) {
+        //     weight.value += t.weight
+        //   }
+        // })
+      } catch (error) {
+        logger.error(error);
+        Pop.toast(error.message, 'error')
       }
+    })
+    return {
+      editable,
+      active: computed(() => AppState.activeProject),
+      tasks: computed(() => AppState.tasks.filter(t => t.sprintId == props.sprint.id)),
+      weight: computed(() => {
+        let weight = 0
+        AppState.tasks.forEach(t => {
+          if (t.sprintId == props.sprint.id) {
+            weight += t.weight
+          }
+        })
+        return weight
+      }),
+
+
+      async remove() {
+        try {
+          const sprintId = props.sprint.id
+          await sprintService.remove(sprintId, route.params.projectId)
+        } catch (error) {
+          logger.error(error)
+        }
+
+      },
+
+      async createTask() {
+        try {
+          editable.value.sprintId = props.sprint.id
+          await taskService.createTask(route.params.projectId, editable.value)
+          // logger.log('createTask', route.params.projectId, editable.value)
+          Pop.toast('created', 'success')
+          editable.value = {}
+        } catch (error) {
+          logger.error(error)
+        }
+      },
+
+
+
     }
   }
+}
 </script>
 
 
 <style lang="scss" scoped>
-  .iSize {
-    height: 10px;
-    width: 10px;
-  }
+.iSize {
+  height: 10px;
+  width: 10px;
+}
 </style>
--- planIt.client/src/pages/ProjectPage.vue
+++ planIt.client/src/pages/ProjectPage.vue
@@ -3,8 +3,13 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-1 p-0 m-0">
-        <div class="btngrad2 bg-cDark text-center text-warning selectable mt-3" type="button" data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        <div
+          class="btngrad2 bg-cDark text-center text-warning selectable mt-3"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasExample"
+          aria-controls="offcanvasExample"
+        >
           P
         </div>
       </div>
@@ -18,11 +23,18 @@
             <small>Created By: {{ active.creator.name }}</small>
           </div>
           <div class="col-md-3">
-            <button class="mdi mdi-delete-forever f-24 btn text-danger" @click="remove(active.id)"></button>
-
+            <button
+              class="mdi mdi-delete-forever f-24 btn text-danger"
+              @click="remove(active.id)"
+            ></button>
           </div>
-          <button class="btn btn-rounded btn-dark mdi mdi-plus ms-3" data-bs-toggle="modal"
-            data-bs-target="#createSprint"></button>
+          <button
+            class="btn btn-rounded btn-dark mdi mdi-plus ms-3 w-50"
+            data-bs-toggle="modal"
+            data-bs-target="#createSprint"
+          >
+            Create Sprint
+          </button>
         </div>
         <h4>Sprints</h4>
         <h6>
@@ -34,87 +46,90 @@
       </div>
       <div class="col-md-1"></div>
     </div>
-
   </div>
 </template>
 
 
 <script>
 
-  import { computed } from "@vue/reactivity"
-  import { AppState } from "../AppState"
-  import { projectService } from "../services/ProjectService"
-  import Pop from "../utils/Pop"
-  import { logger } from "../utils/Logger"
-  import { router } from "../router"
-  import { onMounted } from "@vue/runtime-core"
-  import { useRoute } from "vue-router"
-  import { sprintService } from "../services/SprintService"
-  export default {
-    setup() {
-      const route = useRoute()
-      onMounted(async () => {
+import { computed } from "@vue/reactivity"
+import { AppState } from "../AppState"
+import { projectService } from "../services/ProjectService"
+import Pop from "../utils/Pop"
+import { logger } from "../utils/Logger"
+import { router } from "../router"
+import { onMounted } from "@vue/runtime-core"
+import { useRoute } from "vue-router"
+import { sprintService } from "../services/SprintService"
+export default {
+  setup() {
+    const route = useRoute()
+    onMounted(async () => {
 
+      try {
+        await projectService.getAll('api/projects')
+        await sprintService.getAll('api/projects/' + route.params.projectId + '/sprints')
+      } catch (error) {
+        logger.error(error)
+      }
+    })
+    return {
+      active: computed(() => AppState.projects.find(p => p.id == route.params.projectId)),
+      sprints: computed(() => AppState.sprints),
+
+      async remove(id) {
         try {
-          await projectService.getAll('api/projects')
-          await sprintService.getAll('api/projects/' + route.params.projectId + '/sprints')
+          logger.log('removed')
+          await projectService.remove(id);
+          router.push({
+            name: "Home",
+          })
         } catch (error) {
-          logger.error(error)
-        }
-      })
-      return {
-        active: computed(() => AppState.projects.find(p => p.id == route.params.projectId)),
-        sprints: computed(() => AppState.sprints),
-
-        async remove(id) {
-          try {
-            logger.log('removed')
-            await projectService.remove(id);
-            router.push({
-              name: "Home",
-            })
-          } catch (error) {
-            Pop.toast(error.message, "error");
-          }
+          Pop.toast(error.message, "error");
         }
       }
     }
-
-
   }
+
+
+}
 </script>
 
 
 <style lang="scss" scoped>
-  .btnGrad {
-    background: rgb(145, 8, 243);
-    background: linear-gradient(90deg,
-        rgba(145, 8, 243, 1) 4%,
-        rgba(243, 140, 12, 1) 98%);
-  }
+.btnGrad {
+  background: rgb(145, 8, 243);
+  background: linear-gradient(
+    90deg,
+    rgba(145, 8, 243, 1) 4%,
+    rgba(243, 140, 12, 1) 98%
+  );
+}
 
-  .grad {
-    width: 100%;
-    height: 4px;
-    background: rgb(145, 8, 243);
-    background: linear-gradient(90deg,
-        rgba(145, 8, 243, 1) 4%,
-        rgba(243, 140, 12, 1) 98%);
-  }
+.grad {
+  width: 100%;
+  height: 4px;
+  background: rgb(145, 8, 243);
+  background: linear-gradient(
+    90deg,
+    rgba(145, 8, 243, 1) 4%,
+    rgba(243, 140, 12, 1) 98%
+  );
+}
 
-  .btngrad2 {
-    font-style: italic;
-    /* background: linear-gradient(180deg,
+.btngrad2 {
+  font-style: italic;
+  /* background: linear-gradient(180deg,
         rgba(8, 37, 243, 1) 4%,
         rgba(243, 140, 12, 1) 98%); */
-    /* background-clip: text;
+  /* background-clip: text;
     -webkit-background-clip: text;
     -moz-background-clip: text;
     -moz-text-fill-color: transparent;
     -webkit-text-fill-color: transparent; */
-    font-family: "Bebas Neue", cursive;
-    max-width: 65%;
-    height: 60px;
-    font-size: 3em;
-  }
+  font-family: "Bebas Neue", cursive;
+  max-width: 65%;
+  height: 60px;
+  font-size: 3em;
+}
 </style>
