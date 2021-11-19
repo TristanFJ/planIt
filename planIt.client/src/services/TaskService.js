1d95c32c92ed09457be4a4c828651573063e2357
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2021-11-18 20:01:24 -0700

    Tasks checked

--- planIt.client/src/services/TaskService.js
+++ planIt.client/src/services/TaskService.js
@@ -32,5 +32,16 @@ class TaskService {
     }
     AppState.tasks.splice(index, 1, res.data)
   }
+
+  async completedTask(routeId, id) {
+    // AppState.activeSpell.prepared = !AppState.activeSpell.prepared
+    // await api.put(`api/${AppState.account.name}/spells/${AppState.activeSpell.id}`, AppState.activeSpell)
+    const task = AppState.tasks.find(t => t.id == id)
+    task.isComplete = !task.isComplete
+    logger.log("prepared", task.isComplete)
+    const res = await api.put('api/projects/' + routeId + '/tasks/' + id, task)
+    logger.log(res.data)
+    // AppState.tasks = AppState.tasks
+  }
 }
 export const taskService = new TaskService()
